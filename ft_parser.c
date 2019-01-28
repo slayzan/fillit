e23417f56f7d6c49e2fe88d2800aafc539ac1b77
Hugo MARQUET <humarque@e3r11p4.42.fr>
2019-01-28 13:11:08 +0100

    dss

--- ft_parser.c
+++ ft_parser.c
@@ -6,49 +6,71 @@
 /*   By: humarque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 19:49:36 by humarque          #+#    #+#             */
-/*   Updated: 2019/01/24 19:26:24 by humarque         ###   ########.fr       */
+/*   Updated: 2019/01/28 13:10:58 by humarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fillit.h"
 
-int		ft_checkline(char *line)
+static t_node *ft_puttotab(char *line, t_node lst)
+{
+	int i;
+	static int j;
+	static int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (line[i])
+		lst->tab[j][k++] = line[i++];
+	j++;
+	return (lst);
+}
+
+static int		ft_checkline(char *line)
 {
 	int i;
 
 	i = 0;
-	while (line[i] == '.' || line[i] == '#')
+	while (line[i])
 	{
-		if (line[i] != '.' || line[i] != '#')
+		if(line[i] != '.' || line[i] != '#')
 			return (0);
-		if (i > 4)
+		if(i > 4)
 			return (0);
+		i++;
 	}
+	return (1);
 }
 
+t_node *ft_count(char *line, int count)
+{
+	static t_node *lst;
+	int res; 
+	if(count < 105)
+		printf("trop de tetraminos");
+	else
+	{	
+		if(!(ft_checkline(line)))
+			printf("erreur");
 
-t_node	*ft_stock(const int fd)
-{	
-	char *line;
-	t_node *lst;
-	
-	while (get_next_line(fd, &line) == 1)
-	{
-		while (ret = read(fd, line, 16))
-		{
-			ft_checkline(line);
-		}	
-	}
+	}	
 }
 
 int main(int argc, char **argv)
 {
+	int count;
 	char *line;
 	int fd;
 
+	count = 0;
 	if(!(fd = open(argv[1], O_RDONLY)))
 	{
 		close(fd);
 		exit(EXIT_FAILURE);
 	}
-	ft_stock(fd); 
+	while (get_next_line(fd, &line) == 1)
+	{
+		count++;
+		ft_stock(line, count);
+	}
 }
