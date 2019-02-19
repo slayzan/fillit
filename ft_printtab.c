cad25476875347209a51267b0332c7bad84851d5
Hugo MARQUET <humarque@e1r9p12.42.fr>
2019-02-19 18:55:06 +0100

    printab essais

--- ft_printtab.c
+++ ft_printtab.c
@@ -6,7 +6,11 @@
 /*   By: humarque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 19:51:19 by humarque          #+#    #+#             */
+<<<<<<< HEAD
+/*   Updated: 2019/02/19 18:52:42 by humarque         ###   ########.fr       */
+=======
 /*   Updated: 2019/02/19 18:39:27 by mchamayo         ###   ########.fr       */
+>>>>>>> 909457b0f8afb8d38ea14fe15a82b3ad3b6ee0a6
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +20,23 @@ void	ft_printtab(char ***tab)
 {
 	int num;
 	int x;
+	int y;
 
 	num = 0;
 	x = 0;
+<<<<<<< HEAD
+	y = 0;
+	while (tab[num][x] != 0)
+=======
 
 	while (tab[num][x])
+>>>>>>> 909457b0f8afb8d38ea14fe15a82b3ad3b6ee0a6
 	{
-		printf("%s", tab[num][x]);
+		while (tab[num][x][y] != '\0')
+		{
+			printf("%s", tab[num][x][y++]);
+		}
+		x++;
 	}
 }
 
