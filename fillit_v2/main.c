14a25501c92289af70a38af6f5b6526ee7960c71
Hugo MARQUET <humarque@e1r12p17.42.fr>
2019-03-20 14:48:16 +0100

    main

--- fillit_v2/main.c
+++ fillit_v2/main.c
@@ -1,3 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: humarque <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/20 14:40:37 by humarque          #+#    #+#             */
+/*   Updated: 2019/03/20 14:46:49 by humarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
-int	main(int argc, char **argv);
+int	main(int argc, char **argv)
+{
+	int fd;
+	int fd2;
+	char *line;
+	int count;
+
+	line = NULL;
+	argc = 0;
+	if (!(fd = open(argv[1], O_RDONLY)))
+	{
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	if (!(fd2 = open(argv[1], O_RDONLY)))
+	{
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	if (!(count = ft_parser(fd)))
+		print("error");
+	return (0);
+}
