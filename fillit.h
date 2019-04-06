80967a6c304e72e8e3e795a839adaa5ac7ec1337
Melina CHAMAYOU <mchamayo@e1r11p10.42.fr>
2019-04-06 14:52:54 +0200

    norme

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: humarque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 19:57:23 by humarque          #+#    #+#             */
-/*   Updated: 2019/04/06 14:01:11 by mchamayo         ###   ########.fr       */
+/*   Updated: 2019/04/06 14:51:46 by mchamayo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,44 @@
 # include "libft/get_next_line.h"
 # include <math.h>
 
-typedef struct s_coord
+typedef struct		s_coord
 {
 	int minx;
 	int maxx;
 	int miny;
 	int maxy;
-}			t_coord;
+}					t_coord;
 
-typedef struct s_point
+typedef struct		s_point
 {
 	int x;
 	int y;
-}				t_point;
+}					t_point;
 
-typedef	struct	s_square
+typedef	struct		s_square
 {
-	char **square;
-	int size;
-}				t_square;
+	char	**square;
+	int		size;
+}					t_square;
 
-typedef	struct s_tetri
+typedef	struct		s_tetri
 {
 	char	**shape;
 	t_point	*min;
 	t_point	*max;
 	char	letter;
-} 				t_tetri;
+}					t_tetri;
 
-char **ft_strsplitlol(char const *s, char c);
-void	ft_printresult(char **tab, int len_array);
+void				ft_printresult(char **tab, int len_array);
 char				**read_entry(char *str, int count);
 int					ft_count_tetri(char *str);
-void					ft_tetracking(char ***tetrab, int len_array);
+void				ft_tetracking(char ***tetrab, int len_array);
 int					ft_check_shape(char **tab, int count);
 int					main(int argc, char **argv);
 int					ft_parser(int fd);
 int					ft_sizetetra(char ***array, int bloc, int x, int y);
 char				***ft_tetraclean(char ***tab);
-char 				**ft_stocktetra(int fd, int count);
+char				**ft_stocktetra(int fd, int count);
 void				printtab(char ***tab);
 void				free_square(t_square *square);
 void				ft_printresult(char **tab, int len_array);
@@ -63,7 +62,7 @@ t_point				*new_point(int x, int y);
 t_point				*get_maxpoint(char **tab);
 t_point				*get_minpoint(char **tab);
 t_tetri				**ft_createlist(char **tab, int bloc);
-t_coord				*ft_findpoint(char ***tetrab,int bloc);
+t_coord				*ft_findpoint(char ***tetrab, int bloc);
 t_square			*ft_solve(t_tetri **tetra, int bloc);
 int					main(int argc, char **argv);
 
--- ft_printresult.c
+++ ft_printresult.c
@@ -6,19 +6,21 @@
 /*   By: humarque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 18:46:36 by humarque          #+#    #+#             */
-/*   Updated: 2019/04/03 19:49:06 by humarque         ###   ########.fr       */
+/*   Updated: 2019/04/06 14:49:12 by mchamayo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "fillit.h"
+
 void	ft_printresult(char **tab, int len_array)
 {
 	int i;
-	
+
 	i = 0;
 	while (i < len_array)
 	{
 		ft_putstr(tab[i]);
 		ft_putchar('\n');
 		i++;
-	}	
+	}
 }
